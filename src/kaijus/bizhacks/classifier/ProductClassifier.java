355350bd39c35153280c17c00d57a5843d104641
Zenith08 <kaidenr@shaw.ca>
2020-01-31 22:56:40 -0800

    Did UI Stuff

--- src/kaijus/bizhacks/classifier/ProductClassifier.java
+++ src/kaijus/bizhacks/classifier/ProductClassifier.java
@@ -1,8 +1,36 @@
 package kaijus.bizhacks.classifier;
 
+import kaijus.bizhacks.classifier.ui.ClassifierMenue;
+
 public class ProductClassifier {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		//Starts the UI
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(ClassifierMenue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(ClassifierMenue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(ClassifierMenue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(ClassifierMenue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		}
+		//</editor-fold>
+
+		/* Create and display the form */
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new ClassifierMenue().setVisible(true);
+			}
+		});
 	}
 }
